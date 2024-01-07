204919c9fd41a3fcc7fe863e8106aa9370f6654d
DeFis33 <61848806+DeFis33@users.noreply.github.com>
2024-01-07 03:08:06 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿//***************************************************************************************************
+//***************************************************************************************************
 //* Практическая работа № 17                                                                        *
 //* Выполнил: Пирогов Д., группа 2ИСП                                                               *
 //* Задание: изучить алгоритмы создания классов и их экземпляров, способов их реализации в языке C# *
@@ -6,73 +6,81 @@
 
 using System;
 
-class Building
-{
-    string name;
-    int floors;
-    string Name
-    {
-        get 
-        { 
-            return name; 
-        }
-        set 
-        { 
-            name = value; 
-        }
-    }
-    int Floors
-    {
-        get 
-        { 
-            return floors; 
-        }
-        set
-        {
-            if (value >= 1)
-                floors = value;
-            else Console.WriteLine("Количество этажей должно быть больше 0.");
-        }
-    }
-    public void EnterData()
-    {
-        Console.Write("Введите название здания: ");
-        Name = Console.ReadLine();
-
-        Console.Write("Введите количество этажей: ");
-        try
-        {
-            int a = Convert.ToInt32(Console.ReadLine());
-            Floors = a;
-        }
-        catch (FormatException fe)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"\nОшибка ввода \n" + fe.Message);
-            Console.ForegroundColor = ConsoleColor.Gray;
-        }
-        catch (Exception e)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"\nОшибка ввода \n" + e.Message);
-            Console.ForegroundColor = ConsoleColor.Gray;
-        }
-    }
-    public void DisplayData()
-    {
-        Console.WriteLine($"Название здания: {Name}");
-        Console.WriteLine($"Количество этажей: {Floors}");
-    }
-}
 class Program
 {
+    class Building
+    {
+        string name;
+        int floors;
+        string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        int Floors
+        {
+            get
+            {
+                return floors;
+            }
+            set
+            {
+                if (value >= 1)
+                    floors = value;
+                else Console.WriteLine("Количество этажей должно быть больше 0.");
+            }
+        }
+        public void EnterData()
+        {
+            Console.Write("Введите название здания: ");
+            Name = Console.ReadLine();
+
+            Console.Write("Введите количество этажей: ");
+            try
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                Floors = a;
+            }
+            catch (FormatException fe)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"\nОшибка ввода \n" + fe.Message);
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"\nОшибка ввода \n" + e.Message);
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+        }
+        public void DisplayData()
+        {
+            Console.WriteLine($"Название здания: {Name}");
+            Console.WriteLine($"Количество этажей: {Floors}");
+        }
+        public void Calculation()
+        {
+            int window = Floors * 10;
+            Console.WriteLine($"Окон в вашем здании: {window}");
+        }
+    }
     static void Main()
     {
-        Console.WriteLine("Практическая работа №9.\nЗдравствуйте!");
-        
+        Console.WriteLine("Практическая работа №17.\nЗдравствуйте!");
+
         Building House = new Building();
         House.EnterData();
+        Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("\nИнформация о здании: ");
         House.DisplayData();
+        House.Calculation();
+        Console.ForegroundColor = ConsoleColor.White;
     }
 }
